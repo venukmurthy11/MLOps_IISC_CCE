6ec1b753f3ae607d6cc5a8255079ae7a68cf7c0d
venukmurthy11 <147651495+venukmurthy11@users.noreply.github.com>
2023-11-22 22:29:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+new_app = FastAPI()
 
-@new_app.get("/") def read_root(): return {"Hello": "World"}
+@new_app.get("/")
+def read_root():
+    return {"Hello": "World"}
